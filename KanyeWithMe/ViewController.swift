bb7c4c5e549c0fe0bd954931712481650d44744e
nickybittencourt <nickytuon75@gmail.com>
2020-11-27 03:37:49 -0300

    Initial Commit

---
+++ KanyeWithMe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KanyeWithMe
+//
+//  Created by Nicholas Bittencourt  on 2020-11-27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
